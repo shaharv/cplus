7208b9265d5fab3b37eca6aad978d136b7a465e8
Shahar Valiano <shahar.valiano@takipi.com>
2017-11-01 12:14:11 +0200

    Added Linux version of GetDiskSpace using statvfs

--- cplus/GetDiskSpace.cpp
+++ cplus/GetDiskSpace.cpp
@@ -1,17 +1,24 @@
 #include <cstdint>
 #include <iostream>
+
+#ifdef _WIN32
 #include <windows.h>
+#else
+#include <sys/statvfs.h>
+#endif
 
 using std::cerr;
 using std::cout;
 using std::endl;
+
+#ifdef _WIN32
 
 bool GetDiskSpace(uint64_t& availBytes, uint64_t& totalBytes, uint64_t& totalFreeBytes)
 {
 	ULARGE_INTEGER availBytesPar;
 	ULARGE_INTEGER totalBytesPar;
 	ULARGE_INTEGER totalFreeBytesPar;
-	
+
 	if (!GetDiskFreeSpaceEx(NULL, &availBytesPar, &totalBytesPar, &totalFreeBytesPar))
 	{
 		return false;
@@ -24,15 +31,35 @@ bool GetDiskSpace(uint64_t& availBytes, uint64_t& totalBytes, uint64_t& totalFre
 	return true;
 }
 
+#else
+
+bool GetDiskSpace(uint64_t& availBytes, uint64_t& totalBytes, uint64_t& totalFreeBytes)
+{
+	struct statvfs buffer;
+
+	if (statvfs("/usr", &buffer))
+	{
+		return false;
+	}
+
+	availBytes = static_cast<uint64_t>(buffer.f_bavail * buffer.f_frsize);
+	totalBytes = static_cast<uint64_t>(buffer.f_blocks * buffer.f_frsize);
+	totalFreeBytes = static_cast<uint64_t>(buffer.f_bfree * buffer.f_frsize);
+
+	return true;
+}
+
+#endif
+
 int main()
 {
 	uint64_t availBytes, totalBytes, totalFreeBytes;
-	
+
 	bool result = GetDiskSpace(availBytes, totalBytes, totalFreeBytes);
-	
+
 	if (!result)
 	{
-		cerr << "Error: GetDiskFreeSpaceEx failed." << endl;
+		cerr << "Error: GetDiskSpace failed." << endl;
 		return 0;
 	}
 
@@ -40,5 +67,5 @@ int main()
 	cout << "Total space (bytes):     " << totalBytes << endl;
 	cout << "Free disk space (bytes): " << totalFreeBytes << endl;
 
-	return 0; 
+	return 0;
 }
