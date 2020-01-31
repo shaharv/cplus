7a660c6e1a30125d4fd79f301bae9d390f24fd1d
Shahar Valiano <shahar.valiano@gmail.com>
2020-01-31 11:13:08 +0200

    Add test_queue and test_priority_queue

--- sandbox/ds/std/test_std_containers.cpp
+++ sandbox/ds/std/test_std_containers.cpp
@@ -1,5 +1,6 @@
 #include <list>
 #include <iostream>
+#include <queue>
 #include <stack>
 #include <stdint.h>
 
@@ -25,10 +26,68 @@ void test_stack()
 		std::cout << strings.top() << std::endl;
 		strings.pop();
 	}
+
+	std::cout << std::endl;
+}
+
+void test_priority_queue()
+{
+	const int32_t intArr[] = { 4, -1, 10, 4, 7, 8, 5, 1, 3, 2, 0, 9, 6 };
+	const int32_t intArrSize =  sizeof(intArr) / sizeof(intArr[0]);
+
+	std::priority_queue<int32_t, std::vector<int32_t> > intQueue;
+
+	std::cout << "Pushing elements to priority queue (heap):" << std::endl;
+
+	for (int32_t i = 0; i < intArrSize; i++)
+	{
+		intQueue.push(intArr[i]);
+		std::cout << intArr[i] << std::endl;
+	}
+
+	std::cout << std::endl << "Popping elements from priority queue (heap):" << std::endl;
+
+	while (!intQueue.empty())
+	{
+		std::cout << intQueue.top() << std::endl;
+		intQueue.pop();
+	}
+
+	std::cout << std::endl;
+}
+
+void test_queue()
+{
+	const std::string strArr[] = { "Yael", "Guy", "Galit", "Shahar" };
+
+	std::queue<std::string, std::deque<std::string> > strQueue;
+
+	// Use vector, just for fun
+	const std::vector<std::string> strVec(strArr, strArr + (sizeof(strArr) / sizeof(strArr[0])));
+	std::cout << "Eneueue strings:" << std::endl;
+	for (std::vector<std::string>::const_iterator it = strVec.begin(); it != strVec.end(); ++it)
+	{
+		const std::string& str = *it;
+		strQueue.push(str);
+		std::cout << str << std::endl;
+	}
+
+	std::cout << std::endl << "Dequeue strings:" << std::endl;
+
+	while (!strQueue.empty())
+	{
+		std::cout << strQueue.front() << std::endl;
+		strQueue.pop();
+	}
+
+	std::cout << std::endl;
 }
 
 int main()
 {
 	test_stack();
+	test_priority_queue();
+	test_queue();
+
 	return 0;
 }
